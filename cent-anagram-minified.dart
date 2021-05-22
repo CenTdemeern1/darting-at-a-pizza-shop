38615abd74425b9a143811765f789a49a43107c3
CenTdemeern1 <timo.herngreen@gmail.com>
2021-05-22 14:03:33 +0200

    Minified my code

---
+++ cent-anagram-minified.dart
@@ -0,0 +1,1 @@
+import"dart:io";void main()async{File file=File("words.txt");String contents=await file.readAsString();List<String>words=contents.split("\r\n");for(String word in words){String reversedWord=word.split('').reversed.join();if(words.contains(reversedWord)){print("Anagram found: $word & $reversedWord");}}}
