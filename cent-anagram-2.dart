370fa5aea413aa139ffe318a82be3ae523b50f44
CenTdemeern1 <timo.herngreen@gmail.com>
2021-05-22 14:09:30 +0200

    Misinterpreted the exercise, made a new file following it

---
+++ cent-anagram-2.dart
@@ -0,0 +1,13 @@
+import "dart:io";
+
+void main() async {
+	File file = File("words.txt");
+	String contents = await file.readAsString();
+	List<String> words = contents.split("\r\n");
+	for (String word in words) {
+		String reversedWord = word.split('').reversed.join(); //Splits string into array of letters, then reverses the array, then joins it together.
+		if (word == reversedWord)) {
+			print("Anagram found: $word");
+		}
+	}
+}
