5913ab72e236d9658964b9f5aba2ce72db1360f8
CenTdemeern1 <timo.herngreen@gmail.com>
2021-05-22 14:10:29 +0200

    whoops

--- cent-anagram-2.dart
+++ cent-anagram-2.dart
@@ -6,7 +6,7 @@ void main() async {
 	List<String> words = contents.split("\r\n");
 	for (String word in words) {
 		String reversedWord = word.split('').reversed.join(); //Splits string into array of letters, then reverses the array, then joins it together.
-		if (word == reversedWord)) {
+		if (word == reversedWord) {
 			print("Anagram found: $word");
 		}
 	}
