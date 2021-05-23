0224412bb29ca47cdcde6394c96470d5eff108e1
CenTdemeern1 <timo.herngreen@gmail.com>
2021-05-23 17:21:48 +0200

    Stalin sort

---
+++ stalin-sort.dart
@@ -0,0 +1,22 @@
+// About Stalin Sort
+// Stalin Sort is a joke sorting algorithm that works like follows:
+// Stalin Sort iterates down the list, and checks of the sorted numbers are in order.
+// Any unsorted numbers are eliminated from the list, resulting in what is techinically a sorted list.
+// It's just missing some elements...
+
+void main(List<String> args) {
+	List<int> out = [];
+	bool isFirst = true;
+	for (String item in args) {
+		int number = int.parse(item);
+		if (isFirst) {
+			isFirst = false;
+			out.add(number);
+		} else {
+			if (number >= out.last) {
+				out.add(number);
+			}
+		}
+	}
+	print(out);
+}
